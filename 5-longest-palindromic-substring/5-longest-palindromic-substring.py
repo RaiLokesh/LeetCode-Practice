8a9e2814d18affc41e83b2ec5432f3170d8682ca
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-02-25 14:04:06 +0530

    Time: 3207 ms (23.84%), Space: 13.9 MB (83.38%) - LeetHub

---
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def check_Palindrome(i, j, s, ans):
+        while i>=0 and j<len(s) and s[i]==s[j] :
+            ans = s[i:j+1]
+            i-=1; j+=1
+        return ans
+            
+    def longestPalindrome(self, s: str) -> str:
+        # max of even length palindromes and odd length palindromes
+        # for odd length send i-1, i+1
+        # for even length send i, i+1
+        ans = ''
+        for i in range(len(s)):
+            x = Solution.check_Palindrome(i-1, i+1, s, s[i])
+            y = Solution.check_Palindrome(i, i+1, s, '')
+            if len(x) > len(ans):
+                ans = x
+            if len(y) > len(ans):
+                ans = y
+        return ans
