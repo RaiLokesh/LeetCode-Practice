be5c992c18b3a8b6d38bea13a108a0859819b92c
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-03-01 11:13:22 +0530

    Time: 848 ms (59.24%), Space: 38.2 MB (6.44%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,14 @@
+class Solution:
+    ans = []
+    def partition(self, s: str) -> List[List[str]]:
+        Solution.ans = []
+        def solve(s):
+            arr = []
+            if not s: return [arr]
+            for i in range(len(s)):
+                if s[:i+1] == s[:i+1][::-1]:
+                    x = solve(s[i+1:])
+                    for j in x:
+                        arr.append([s[:i+1]]+j)
+            return arr
+        return solve(s)
