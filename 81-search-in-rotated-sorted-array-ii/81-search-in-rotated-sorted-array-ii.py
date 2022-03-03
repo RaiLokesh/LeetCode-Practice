e47e00f883f3ec4ef5953ad1f84b7d74cb9f9e2a
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-03-03 11:29:49 +0530

    Time: 367 ms (5.12%), Space: 14.5 MB (80.14%) - LeetHub

---
+++ 81-search-in-rotated-sorted-array-ii/81-search-in-rotated-sorted-array-ii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        
+        l = []
+        for i in nums:
+            if i not in l: l.append(i)
+        # step 1 : find the pivot point using binary search
+        i = 0
+        j = len(l) -1
+        start = 0
+        end = len(l) -1 
+        if l[0] > l[-1]:
+            pp = -1
+            while i < j:
+                mid = (i+j) // 2
+                if l[mid] < l[mid-1]:
+                    pp = mid
+                    break
+                elif l[mid] > l[mid+1]:
+                    pp = mid+1
+                    break
+                elif l[mid] > l[j]:
+                    i = mid + 1
+                elif l[mid] < l[i]:
+                    j = mid - 1
+            if target < l[0]:
+                start = pp
+            else:
+                end = pp-1
+                
+        # step 2 : binary search in the list to find the target
+        
+        while start <= end:
+            mid = (start+end)//2
+            if target == l[mid]:
+                return True
+            elif target < l[mid]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return False
