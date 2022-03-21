cf0146c3c07c99cbbaebeed1f4f80f8cfe6f9f00
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-03-21 23:30:53 +0530

    Time: 181 ms (51.40%), Space: 14.8 MB (63.96%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -4,14 +4,34 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        row, col = dd(lambda:0), dd(lambda:0)
-        m = len(matrix)
-        n = len(matrix[0])
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j]==0:
-                    row[i]=1
-                    col[j]=1
-        for i in range(m):
-            for j in range(n):
-                if row[i] or col[j] : matrix[i][j]=0
+        col_flag = False
+        row_flag = False
+        
+        for i in range(len(matrix[0])):
+            if matrix[0][i] == 0:
+                row_flag = True
+                
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                col_flag = True
+        
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][j] == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+        
+        for i in range(1, len(matrix)):
+            for j in range(1, len(matrix[0])):
+                if matrix[i][0] == 0:
+                    matrix[i][j] = 0
+                if matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        
+        for i in range(len(matrix[0])):
+            if row_flag:
+                matrix[0][i] = 0
+                
+        for i in range(len(matrix)):
+            if col_flag:
+                matrix[i][0] = 0
