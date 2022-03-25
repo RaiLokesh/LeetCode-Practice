0bce239005c1de13261f9767c22685f58836d1e7
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-03-25 19:02:49 +0530

    Time: 969 ms (29.53%), Space: 15.3 MB (94.98%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,32 @@
+class Solution:
+    grid = []
+    def dfs(self, i, j):
+        Solution.grid[i][j] = 0
+        if i+1 < len(Solution.grid) and Solution.grid[i+1][j] == 1:
+            self.dfs(i+1, j)
+        if j+1 < len(Solution.grid[0]) and Solution.grid[i][j+1] == 1:
+            self.dfs(i, j+1)
+        if i-1 >= 0 and Solution.grid[i-1][j] == 1:
+            self.dfs(i-1, j)
+        if j-1 >= 0 and Solution.grid[i][j-1] == 1:
+            self.dfs(i, j-1)
+    
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        Solution.grid = grid
+        
+        for i in range(len(grid)):
+            if grid[i][0] == 1:
+                self.dfs(i, 0)
+            if grid[i][len(grid[0])-1] == 1:
+                self.dfs(i, len(grid[0])-1)
+        
+        for i in range(len(grid[0])):
+            if grid[0][i] == 1:
+                self.dfs(0, i)
+            if grid[len(grid)-1][i] == 1:
+                self.dfs(len(grid)-1, i)
+        
+        ans = 0
+        for i in grid:
+            ans += i.count(1)
+        return ans
