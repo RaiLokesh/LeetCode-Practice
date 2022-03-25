1ca2968a4b58b63523fe0f3387478b911bf091f4
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-03-25 23:15:09 +0530

    Time: 58 ms (20.68%), Space: 13.9 MB (28.98%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,5 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        '''
         if len(nums) == 1: return nums[0]
         dp = [0]*len(nums)
         dp[0] = nums[0]
@@ -8,3 +9,20 @@ class Solution:
             for j in range(0, i-1):
                 dp[i] = max(dp[i], nums[i] + dp[j])
         return(max(dp[-1], dp[-2]))
+        '''
+        dp = [0]*len(nums)
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        if len(nums) == 3:
+            return max(nums[0] + nums[2], nums[1])
+        
+        dp[0] = nums[0]
+        dp[1] = nums[1]
+        dp[2] = nums[2] + nums[0]
+        for i in range(3, len(nums)):
+            dp[i] = nums[i] + max(dp[i-2], dp[i-3])
+        return max(dp)
