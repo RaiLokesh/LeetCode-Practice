0fff6eb3333b57255a9a857a584c41daf5cb3830
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-04-06 13:38:35 +0530

    Time: 80 ms (72.11%), Space: 13.9 MB (47.37%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,24 +5,35 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        x1 = ''
-        x2 = ''
-        while l1!=None:
-            x1+=str(l1.val)
-            l1=l1.next
-        while l2!=None:
-            x2+=str(l2.val)
-            l2=l2.next
-        x1=x1[::-1]
-        x2=x2[::-1]
-        y = (int(x1)+int(x2))
-        x = ListNode(0)
-        if y==0: return x
-        start=x
-        while y!=0:
-            x3=y%10
-            a = ListNode(x3)
-            x.next=a
-            x=a
-            y//=10
-        return start.next
+        ptr1 = l1
+        ptr2 = l2
+        while ptr1 and ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+        if ptr2:
+            l1, l2 = l2, l1
+        ptr1 = l1
+        ptr2 = l2
+        carry = 0
+        while ptr1:
+            if ptr2:
+                sm = (ptr1.val + ptr2.val + carry)
+                ptr1.val = sm % 10
+                carry = sm // 10
+            else:
+                sm = (ptr1.val + carry)
+                ptr1.val = sm % 10
+                carry = sm // 10
+            ptr1 = ptr1.next
+            if ptr2: ptr2 = ptr2.next
+        
+        if carry:
+            carry = str(carry)
+            ptr1 = l1
+            while ptr1.next: ptr1 = ptr1.next
+            for i in carry:
+                temp = ListNode(int(i))
+                ptr1.next = temp
+                ptr1 = ptr1.next
+                
+        return l1
