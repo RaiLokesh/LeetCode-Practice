88d96da28f6892209c0b3870ecdcab88baf09305
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-04-08 18:22:44 +0530

    Time: 992 ms (5.03%), Space: 20.9 MB (99.38%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.py
@@ -3,8 +3,6 @@ class Solution:
         
         for i in range(len(nums)):
             nums[(nums[i]-1)%len(nums)] = nums[(nums[i]-1)%len(nums)] + len(nums)
-        ans = []
         for i in range(len(nums)):
             if nums[i] > 2*len(nums):
-                ans.append(i+1)
-        return ans
+                yield(i+1)
