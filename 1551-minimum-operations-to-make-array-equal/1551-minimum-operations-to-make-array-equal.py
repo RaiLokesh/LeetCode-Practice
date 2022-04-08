744699002c61992aee4faf8d4a2882922dcb1d11
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-04-08 09:48:04 +0530

    Time: 87 ms (43.70%), Space: 13.9 MB (69.44%) - LeetHub

---
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minOperations(self, n: int) -> int:
+        flag = ans = 0
+        if n % 2:
+            flag = 2 * (n // 2) + 1
+        else:
+            flag = (2 * (n // 2) + 1 + 2 * (n // 2 - 1) + 1) // 2
+        for i in range(n//2):
+            ans += flag - (2 * i + 1)
+        return ans
