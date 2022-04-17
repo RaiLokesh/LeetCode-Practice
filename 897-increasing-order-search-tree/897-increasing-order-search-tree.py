12331939dabb745e1c727ab7cb97c9d01fe6f8fa
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-04-17 09:35:56 +0530

    Time: 38 ms (68.56%), Space: 13.8 MB (99.23%) - LeetHub

---
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    head = None
+    pt = None
+    def traverse(self, ptr):
+        if ptr.left:
+            self.traverse(ptr.left)
+            
+        if Solution.head:
+            Solution.pt.right = TreeNode(ptr.val)
+            Solution.pt = Solution.pt.right
+            
+        if not Solution.head:
+            Solution.head = Solution.pt = ptr
+            
+        if ptr.right:
+            self.traverse(ptr.right)
+        
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        Solution.head = None
+        self.traverse(root)
+        return Solution.head
