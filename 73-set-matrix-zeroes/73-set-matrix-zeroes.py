18f5d79e405231785ffb544a84cfa51cabf076f2
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-07-07 12:23:09 +0530

    Time: 123 ms (99.07%), Space: 14.7 MB (89.99%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.py
@@ -1,37 +1,17 @@
-from collections import defaultdict as dd
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        col_flag = False
-        row_flag = False
-        
-        for i in range(len(matrix[0])):
-            if matrix[0][i] == 0:
-                row_flag = True
-                
+        row = set()
+        col = set()
         for i in range(len(matrix)):
-            if matrix[i][0] == 0:
-                col_flag = True
-        
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
+            for j in range(len(matrix[0])):
                 if matrix[i][j] == 0:
-                    matrix[0][j] = 0
-                    matrix[i][0] = 0
+                    row.add(i)
+                    col.add(j)
         
-        for i in range(1, len(matrix)):
-            for j in range(1, len(matrix[0])):
-                if matrix[i][0] == 0:
-                    matrix[i][j] = 0
-                if matrix[0][j] == 0:
-                    matrix[i][j] = 0
-        
-        for i in range(len(matrix[0])):
-            if row_flag:
-                matrix[0][i] = 0
-                
         for i in range(len(matrix)):
-            if col_flag:
-                matrix[i][0] = 0
+            for j in range(len(matrix[0])):
+                if i in row or j in col:
+                    matrix[i][j] = 0
