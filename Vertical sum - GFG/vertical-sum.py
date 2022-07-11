19fe0dc9a02f722b689a5e269643bef25211ae04
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-07-11 19:37:11 +0530

    Added solution - LeetHub

---
+++ Vertical sum - GFG/vertical-sum.py
@@ -0,0 +1,109 @@
+#{ 
+#Driver Code Starts
+#Initial Template for Python 3
+
+from collections import deque
+# Tree Node
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+
+# Function to Build Tree   
+def buildTree(s):
+    #Corner Case
+    if(len(s)==0 or s[0]=="N"):           
+        return None
+        
+    # Creating list of strings from input 
+    # string after spliting by space
+    ip=list(map(str,s.split()))
+    
+    # Create the root of the tree
+    root=Node(int(ip[0]))                     
+    size=0
+    q=deque()
+    
+    # Push the root to the queue
+    q.append(root)                            
+    size=size+1 
+    
+    # Starting from the second element
+    i=1                                       
+    while(size>0 and i<len(ip)):
+        # Get and remove the front of the queue
+        currNode=q[0]
+        q.popleft()
+        size=size-1
+        
+        # Get the current node's value from the string
+        currVal=ip[i]
+        
+        # If the left child is not null
+        if(currVal!="N"):
+            
+            # Create the left child for the current node
+            currNode.left=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.left)
+            size=size+1
+        # For the right child
+        i=i+1
+        if(i>=len(ip)):
+            break
+        currVal=ip[i]
+        
+        # If the right child is not null
+        if(currVal!="N"):
+            
+            # Create the right child for the current node
+            currNode.right=Node(int(currVal))
+            
+            # Push it to the queue
+            q.append(currNode.right)
+            size=size+1
+        i=i+1
+    return root
+    
+
+ # } Driver Code Ends
+#User function Template for python3
+from collections import defaultdict as dd
+'''
+# Node Class:
+class Node:
+    def __init__(self,val):
+        self.data = val
+        self.left = None
+        self.right = None
+'''
+class Solution:
+    #Complete the function below
+    d = dd(lambda:0)
+    def goSolve(self, ptr, sm):
+        Solution.d[sm] += ptr.data
+        if ptr.left: self.goSolve(ptr.left, sm-1)
+        if ptr.right: self.goSolve(ptr.right, sm+1)
+    def verticalSum(self, root):
+        
+        Solution.d = dd(lambda:0)
+        self.goSolve(root, 0)
+        ans =sorted(Solution.d.items())
+        p = []
+        for i in ans:
+            p.append(i[1])
+        return p
+#{ 
+#Driver Code Starts.
+if __name__=="__main__":
+    t=int(input())
+    for _ in range(0,t):
+        s=input()
+        root=buildTree(s)
+        res = Solution().verticalSum(root)
+        for i in res:
+            print (i, end = " ")
+        print();
+#} Driver Code Ends
