a0a823971cc3a77d9e03dd58b9a81b1fe56dd4ea
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-07-16 20:12:15 +0530

    Time: 407 ms (77.10%), Space: 25.1 MB (13.98%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.py
@@ -0,0 +1,27 @@
+from collections import defaultdict as dd
+
+l = []
+d = dd(lambda:0)
+i = 1
+while i**2 <= 10001:
+    l.append(i**2)
+    i += 1
+def solve(sm):
+    if sm == 0:
+        return 1
+    elif sm < 0: return 100000
+    else:
+        if d[sm]: return d[sm]
+        ct = 100000
+        for i in range(len(l)):
+            ct = min(ct, 1 + solve(sm - l[i]))
+        d[sm] = ct
+        return ct
+
+solve(10001)
+
+class Solution:
+    def numSquares(self, n: int) -> int:
+        # -> run a loop to n and keep track of all i^2 values <= n
+        # -> this problem is now transformed into coin problem (standard dp)
+        return d[n]-1
