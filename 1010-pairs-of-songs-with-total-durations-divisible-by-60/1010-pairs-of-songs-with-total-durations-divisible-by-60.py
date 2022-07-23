164badb2c2175c26506df63e1e8bcd964e55faa8
Lokesh Rai <lokeshrai2019@iiitkottayam.ac.in>
2022-07-23 20:25:19 +0530

    Time: 475 ms (36.72%), Space: 18.3 MB (36.97%) - LeetHub

--- 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
+++ 1010-pairs-of-songs-with-total-durations-divisible-by-60/1010-pairs-of-songs-with-total-durations-divisible-by-60.py
@@ -1,11 +1,10 @@
 from collections import defaultdict as dd
 class Solution:
     def numPairsDivisibleBy60(self, time: List[int]) -> int:
-        array = [60, 120, 180, 240, 300, 360, 420, 480, 540, 600, 660, 720, 780, 840, 900, 960, 1020]
-        
+        array = [0, 60]
+        time = [i%60 for i in time]
         d = dd(lambda:0)
         ans = 0
-        
         for i in time:
             ans += d[i]
             for j in array:
